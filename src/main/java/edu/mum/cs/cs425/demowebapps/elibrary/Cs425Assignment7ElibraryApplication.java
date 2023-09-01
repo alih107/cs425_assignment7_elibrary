3861bb307f958f4dbd163d4d65896234bd55fad9
aamandyk <alikhan.amandyk@dnapayments.com>
2023-08-31 20:16:20 -0400

    first commit

---
+++ src/main/java/edu/mum/cs/cs425/demowebapps/elibrary/Cs425Assignment7ElibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.elibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs425Assignment7ElibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs425Assignment7ElibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/mum/cs/cs425/demowebapps/elibrary/Cs425Assignment7ElibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mum.cs.cs425.demowebapps.elibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs425Assignment7ElibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
